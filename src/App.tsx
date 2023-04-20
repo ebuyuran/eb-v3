aa3266ee6478efaa6bf068b0e2ab0788a0534acd
ebuyuran <efebuyuran@gmail.com>
2023-04-20 08:44:27 +0300

    test

--- src/App.tsx
+++ src/App.tsx
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Personal Website of Efe Buyuran
+          My personal website
         </p>
         <a
           className="App-link"
